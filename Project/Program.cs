afee8d7d0237034e5cea4ed00819c4414f18361a
Andrey Perevertov <gociioduh@gmail.com>
2023-03-26 22:27:05 +0300

    implementation of the solution in C#

---
+++ Project/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+int size = array.Length;
+int index = 0;
+int indexNew = 0;
+for (index = 0; index < size; index++)
+{
+    if (array[index].Length < 4) indexNew++;
+}
+string[] arrayItog = new string[indexNew];
+index = 0;
+int i = 0;
+for (index = 0; index < size; index++)
+{
+    if (array[index].Length < 4)
+    {
+        arrayItog[i] = array[index];
+        i++;
+    }
+}
+Console.WriteLine("[" + string.Join(", ", arrayItog) + "]");
+
+
